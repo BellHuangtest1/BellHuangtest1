b469333e23fc994d746aa7890efd46119e9091a5
root <root@fvgbn.jgacahsrgcxupnrgjbwhaaareb.hx.internal.cloudapp.net>
2019-11-25 11:57:17 +0000

    first commit

---
+++ 1.tf
@@ -0,0 +1,13 @@
+vi alicloud-terraform-infra/disney_shdr_technology/stage/applications/tech-qa-app/security_groups.tf
+ 
+resource "alicloud_security_group_rule" "appd-tcp-out12" {
+type = "egress"
+ip_protocol = "tcp"
+nic_type = "intranet"
+policy = "accept"
+port_range = "1688/1688"
+priority = "1"
+security_group_id = "${alicloud_security_group.group.0.id}"
+cidr_ip = "10.143.32.28/32"
+} 
+
